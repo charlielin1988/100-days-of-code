22bc18b62649870de4a06f607a86e9fce27a1c92
shelbylin13 <shelbylin13@gmail.com>
2022-07-18 10:24:06 -0500

    JS math basics:

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-var favoriteFood = 'pizza';
+// var favoriteFood = 'pizza';
 
-var numOfSlices = 8;
-console.log(favoriteFood);
-console.log(numOfSlices);
+// var numOfSlices = 8;
+// console.log(favoriteFood);
+// console.log(numOfSlices);
 
-let changeMe = true;
-changeMe = false;
-console.log(changeMe);
+// let changeMe = true;
+// changeMe = false;
+// console.log(changeMe);
 
-const entree = 'Enchiladas';
+// const entree = 'Enchiladas';
+// console.log(entree);
+
+// let levelUp = 0;
+// console.log(levelUp);
+// levelUp += 5;
+// console.log(levelUp);
+
+// let powerLevel = 200;
+// powerLevel -= 100;
+// console.log(powerLevel);
+
+let quarterMe = 400;
+quarterMe /= 4;
+console.log(quarterMe);
