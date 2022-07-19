5eb8045fcb732df5f3a0b1011efc931d2f1aa5b4
shelbylin13 <shelbylin13@gmail.com>
2022-07-19 12:35:00 -0500

    finished truthy and falsy exercise 2

--- conditionals.js
+++ conditionals.js
@@ -26,9 +26,33 @@
 //   console.log(`Great! You've started your work!`);
 // }
 
-let favoritePhrase = '';
-if (favoritePhrase) {
-  console.log('Cool cool cool');
-} else {
-  console.log('This string is definitely empty.');
-}
+// let favoritePhrase = '';
+// if (favoritePhrase) {
+//   console.log('Cool cool cool');
+// } else {
+//   console.log('This string is definitely empty.');
+// }
+
+// let username = '';
+// checks for empty string
+// let defaultName;
+// stores defaultName as a variable
+// if (username) {
+//   defaultName = username;
+// } else {
+//   defaultName = 'Stranger';
+// }
+// uses falsy and truthy conditionals to check if there is a username. If the string is empty, it will assign it the default name of 'Stranger'
+// console.log(defaultName);
+
+// we can abbreviate this code to the following
+
+// let username = '';
+// let defaultName = username || 'Stranger';
+//  let the default name be the username, unless the username is empty, then assign the default name to 'Stranger'
+// console.log(defaultName);
+
+// let tool = 'marker';
+// let writingUtensil = tool || 'pen';
+
+// console.log(`The ${writingUtensil} is mightier than the sword.`);
