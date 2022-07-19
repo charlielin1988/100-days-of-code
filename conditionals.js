140ab18e9fc756b796466743e13f00593f5527f0
shelbylin13 <shelbylin13@gmail.com>
2022-07-19 12:12:37 -0500

    finished comparison exercise

--- conditionals.js
+++ conditionals.js
@@ -1,7 +1,14 @@
-let sale = true;
-sale = false;
-if (sale === true) {
-  console.log('Time to buy!');
+// let sale = true;
+// sale = false;
+// if (sale === true) {
+//   console.log('Time to buy!');
+// } else {
+//   console.log('Time to wait for a sale.');
+// }
+
+let hungerLevel = 7;
+if (hungerLevel > 7) {
+  console.log('Time to eat!');
 } else {
-  console.log('Time to wait for a sale.');
+  console.log('We can eat later!');
 }
