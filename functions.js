63873d741ae4e260d84d1a59b97e329fab28182e
shelbylin13 <shelbylin13@gmail.com>
2022-07-21 14:54:57 -0500

    function basics exercise 1

---
+++ functions.js
@@ -0,0 +1,18 @@
+// *****function basics******
+
+function getReminder() {
+  console.log('Water the plants.');
+}
+// getReminder();
+
+function greetInSpanish() {
+  console.log('Buenas Tardes.');
+}
+// greetInSpanish();
+
+function sayThanks() {
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+// sayThanks();
+
+// *****parameters and arguments*****
