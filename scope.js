f4832138eb835a19e4e40802baa388563ee9e6c6
shelbylin13 <shelbylin13@gmail.com>
2022-07-22 14:05:19 -0500

    first exercise of scope lesson

---
+++ scope.js
@@ -0,0 +1,19 @@
+// the body of a function is a block
+
+// const logSkyColor = () => {
+//   let color = 'blue';
+//   console.log(color);
+// }
+
+// the body of an if statement is a block
+// if(dusk) {
+//   let color = 'pink';
+//   console.log(color);
+// }
+
+const city = 'New York City';
+logCitySkyline = () => {
+  let skyscraper = 'Empire State Building';
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+};
+console.log(logCitySkyline());
