fa5e607dccb4099dd7492384b1287b2f06f58368
shelbylin13 <shelbylin13@gmail.com>
2022-07-22 14:13:11 -0500

    second exercise of scope review

--- scope.js
+++ scope.js
@@ -1,3 +1,5 @@
+// **** intro to scope ****
+
 // the body of a function is a block
 
 // const logSkyColor = () => {
@@ -11,9 +13,29 @@
 //   console.log(color);
 // }
 
-const city = 'New York City';
-logCitySkyline = () => {
-  let skyscraper = 'Empire State Building';
-  return 'The stars over the ' + skyscraper + ' in ' + city;
+// const city = 'New York City';
+// logCitySkyline = () => {
+//   let skyscraper = 'Empire State Building';
+//   return 'The stars over the ' + skyscraper + ' in ' + city;
+// };
+// console.log(logCitySkyline());
+
+// the function logCitySkyline can access both city variable and the skyscraper variable
+
+// *** global scope***
+
+// const color = 'blue';
+// const returnSkyColor = () => {
+//   return color;
+// };
+// console.log(returnSkyColor());
+// the variable color exists outside of the function block, but can be accessed by the function because it has global scope
+
+let satellite = 'The Moon';
+let galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const callMyNightSky = () => {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
 };
-console.log(logCitySkyline());
+console.log(callMyNightSky());
