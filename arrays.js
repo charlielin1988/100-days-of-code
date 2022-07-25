5ee7343627956d92c7043fab634025ff5875bca6
shelbylin13 <shelbylin13@gmail.com>
2022-07-25 16:33:22 -0500

    accessing items in an array exercise

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-// intro to arrays
+// **** intro to arrays ****
 // example 1:
 
 // let newYearsResolutions = [
@@ -8,5 +8,17 @@
 // ];
 // console.log(newYearsResolutions);
 
-const hobbies = ['reading', 'baking', 'witchcraft'];
-console.log(hobbies);
+// const hobbies = ['reading', 'baking', 'witchcraft'];
+// console.log(hobbies);
+
+// **** accessing items in an array ****
+
+// const famousSayings = [
+//   'Fortune favors the brave.',
+//   'A joke is a very serious thing.',
+//   'Where there is love there is life.'
+// ];
+
+// let listItem = famousSayings[0];
+// console.log(famousSayings[0]);
+// console.log(famousSayings[2]);
