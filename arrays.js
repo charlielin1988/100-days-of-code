b34587cebeff1c7fd85bc123b3b1b44e8ae06537
shelbylin13 <shelbylin13@gmail.com>
2022-07-25 16:29:40 -0500

    arrays exercise 1

---
+++ arrays.js
@@ -0,0 +1,12 @@
+// intro to arrays
+// example 1:
+
+// let newYearsResolutions = [
+//   'Keep a journal',
+//   'Take a falconry class',
+//   'Learn to juggle'
+// ];
+// console.log(newYearsResolutions);
+
+const hobbies = ['reading', 'baking', 'witchcraft'];
+console.log(hobbies);
