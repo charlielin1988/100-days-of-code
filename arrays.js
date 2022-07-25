5d5ba6ef8f3cfae90c6374128c6f9feae7337ec1
shelbylin13 <shelbylin13@gmail.com>
2022-07-25 17:06:34 -0500

    arrays and functions

--- arrays.js
+++ arrays.js
@@ -65,22 +65,37 @@
 
 // *** more array methods ***
 
-const groceryList = [
-  'orange juice',
-  'bananas',
-  'coffee beans',
-  'brown rice',
-  'pasta',
-  'coconut oil',
-  'plantains'
-];
-groceryList.shift();
+// const groceryList = [
+//   'orange juice',
+//   'bananas',
+//   'coffee beans',
+//   'brown rice',
+//   'pasta',
+//   'coconut oil',
+//   'plantains'
+// ];
+// groceryList.shift();
 // console.log(groceryList);
-groceryList.unshift('popcorn');
+// groceryList.unshift('popcorn');
 // console.log(groceryList);
 
 // console.log(groceryList.slice(1, 4));
 
-groceryList.indexOf('pasta');
-const pastaIndex = groceryList.indexOf('pasta');
-console.log(pastaIndex);
+// groceryList.indexOf('pasta');
+// const pastaIndex = groceryList.indexOf('pasta');
+// console.log(pastaIndex);
+
+// *** arrays and functions ***
+
+// const concept = ['arrays', 'can', 'be', 'mutated'];
+// function changeArr(arr) {
+//   arr[3] = 'MUTATED';
+// }
+// changeArr(concept);
+// console.log(concept);
+
+// function removeElement(newArr) {
+//   newArr.pop();
+// }
+// removeElement(concept);
+// console.log(concept);
