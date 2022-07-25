4f637cbcc6d1233670f7db86295fe436ae7e5b2e
shelbylin13 <shelbylin13@gmail.com>
2022-07-25 17:10:43 -0500

    nested arrays

--- arrays.js
+++ arrays.js
@@ -99,3 +99,13 @@
 // }
 // removeElement(concept);
 // console.log(concept);
+
+// *** nested arrays ***
+
+const numberClusters = [
+  [1, 2],
+  [3, 4],
+  [5, 6]
+];
+const target = numberClusters[2][1];
+console.log(target);
