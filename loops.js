2ded04ab8ff365635ca0503b47499375b8ed6bb4
shelbylin13 <shelbylin13@gmail.com>
2022-07-26 14:40:47 -0500

    nested loops

--- loops.js
+++ loops.js
@@ -12,6 +12,26 @@
 //   console.log(counter);
 // }
 
-for (let counter = 3; counter >= 0; counter--) {
-  console.log(counter);
+// for (let counter = 3; counter >= 0; counter--) {
+//   console.log(counter);
+// }
+
+// **** Looping Through Arrays ***
+// const vacationSpots = ['Morocco', 'Prague', 'Ireland'];
+// for (let i = 0; i < vacationSpots.length; i++) {
+//   console.log('I would like to visit ' + vacationSpots[i]);
+// }
+
+// *** Nested Loops **
+const bobsFollowers = ['rudy', 'gene', 'linda', 'louise'];
+const tinasFollowers = ['gene', 'jimmyjr', 'sara', 'linda'];
+let mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
 }
+console.log(mutualFollowers);
