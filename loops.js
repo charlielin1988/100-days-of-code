ca431ca50836a762933718cb8c642701a2822b68
shelbylin13 <shelbylin13@gmail.com>
2022-07-26 15:41:22 -0500

    the break keyword

--- loops.js
+++ loops.js
@@ -44,10 +44,21 @@
 //   console.log(currentCard);
 // }
 //  **** do...while statements ****
-let cupsOfSugarNeeded = 4;
-let cupsAdded = 0;
+// let cupsOfSugarNeeded = 4;
+// let cupsAdded = 0;
 
-do {
-  cupsAdded++;
-  console.log(cupsAdded);
-} while (cupsAdded < cupsOfSugarNeeded);
+// do {
+//   cupsAdded++;
+//   console.log(cupsAdded);
+// } while (cupsAdded < cupsOfSugarNeeded);
+
+// *** the break keyword ***
+
+const rapperArray = ["Lil' Kim", 'Jay-Z', 'Notorious B.I.G', 'Tupac'];
+for (let i = 0; i < rapperArray.length; i++) {
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === 'Notorious B.I.G') {
+    break;
+  }
+}
+console.log("And if you don't know, now you know");
