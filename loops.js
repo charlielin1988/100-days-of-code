08bbc51f302692cb70c592b8c53dd5f2927ad3f1
shelbylin13 <shelbylin13@gmail.com>
2022-07-26 15:44:18 -0500

    saving loops changes

--- loops.js
+++ loops.js
@@ -54,11 +54,11 @@
 
 // *** the break keyword ***
 
-const rapperArray = ["Lil' Kim", 'Jay-Z', 'Notorious B.I.G', 'Tupac'];
-for (let i = 0; i < rapperArray.length; i++) {
-  console.log(rapperArray[i]);
-  if (rapperArray[i] === 'Notorious B.I.G') {
-    break;
-  }
-}
-console.log("And if you don't know, now you know");
+// const rapperArray = ["Lil' Kim", 'Jay-Z', 'Notorious B.I.G', 'Tupac'];
+// for (let i = 0; i < rapperArray.length; i++) {
+//   console.log(rapperArray[i]);
+//   if (rapperArray[i] === 'Notorious B.I.G') {
+//     break;
+//   }
+// }
+// console.log("And if you don't know, now you know");
