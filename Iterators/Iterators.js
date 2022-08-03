39bc3b05df785b794b6554d679a5837f08080d20
shelbylin13 <shelbylin13@gmail.com>
2022-08-03 16:53:19 -0500

    iterators for each

---
+++ Iterators/Iterators.js
@@ -0,0 +1,6 @@
+// *** the .forEach() method ***
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+fruits.forEach((fruit) => console.log('I want to eat a(n) ' + fruit));
+
+//  *** the .map() method ***
