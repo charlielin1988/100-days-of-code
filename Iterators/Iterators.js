98ed7db6d9df893d9c6acd1d974be814e7db674f
shelbylin13 <shelbylin13@gmail.com>
2022-08-03 17:47:25 -0500

    reduce methods

--- Iterators/Iterators.js
+++ Iterators/Iterators.js
@@ -73,4 +73,12 @@
 // });
 // console.log(startsWithS);
 
-//
+// *** the .reduce() Method ***
+
+// const newNumbers = [1, 3, 5, 7];
+// const newSum = newNumbers.reduce((accumulator, currentValue) => {
+//   console.log('The value of accumulator: ', accumulator);
+//   console.log('The value of currentValue: ', currentValue);
+//   return accumulator + currentValue;
+// }, 10);
+// console.log(newSum);
