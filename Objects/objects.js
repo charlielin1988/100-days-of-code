0644951a7d747514fba98b63a6244b40e467f8b0
shelbylin13 <shelbylin13@gmail.com>
2022-08-23 11:33:19 -0500

    intro to objects created

---
+++ Objects/objects.js
@@ -0,0 +1,13 @@
+// Intro To Objects
+
+// let spaceship = {
+//   'Fuel Type': 'diesel',
+//   color: 'silver'
+// };
+
+// let fasterShip = {
+//   'Fuel Type': 'Turbo Fuel',
+//   color: 'silver'
+// };
+
+// Accessing Properties
