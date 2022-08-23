1cd723b032ea88d0e55e01576217fe1278430f1a
shelbylin13 <shelbylin13@gmail.com>
2022-08-23 15:58:27 -0500

    nested objects

--- Objects/objects.js
+++ Objects/objects.js
@@ -55,15 +55,57 @@
 
 // Methods
 
-let retreatMessage =
-  'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
-let alienShip = {
-  retreat() {
-    console.log(retreatMessage);
+// let retreatMessage =
+//   'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+// let alienShip = {
+//   retreat() {
+//     console.log(retreatMessage);
+//   },
+//   takeoff() {
+//     console.log('Spim...Borp..Glix...Blastoff!');
+//   }
+// };
+// alienShip.retreat();
+// alienShip.takeoff();
+
+// Nested Objects
+
+let spaceship = {
+  passengers: null,
+  telescope: {
+    yearBuilt: 2018,
+    model: '91031-XLT',
+    focalLength: 2032
   },
-  takeoff() {
-    console.log('Spim...Borp..Glix...Blastoff!');
+  crew: {
+    captain: {
+      name: 'Sandra',
+      degree: 'Computer Engineering',
+      encourageTeam() {
+        console.log('We got this!');
+      },
+      'favorite foods': ['cookies', 'cakes', 'candy', 'spinach']
+    }
+  },
+  engine: {
+    model: 'Nimbus2000'
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: 'HD'
+    },
+    'back-up': {
+      battery: 'Lithium',
+      terabytes: 50
+    }
   }
 };
-alienShip.retreat();
-alienShip.takeoff();
+
+let capFave = spaceship.crew.captain['favorite foods'][0];
+console.log(capFave);
+
+spaceship.passengers = [{ firstName: 'John' }, {}];
+
+let firstPassenger = spaceship.passengers[0];
+console.log(firstPassenger);
