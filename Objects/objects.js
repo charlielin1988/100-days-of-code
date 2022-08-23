303dea4d0e2f161c3ce206486036669e424b9e79
shelbylin13 <shelbylin13@gmail.com>
2022-08-23 15:47:29 -0500

    methods

--- Objects/objects.js
+++ Objects/objects.js
@@ -40,15 +40,30 @@
 // console.log(spaceship[propName]);
 
 // Property Assignment
-let spaceship = {
-  'Fuel Type': 'Turbo Fuel',
-  homePlanet: 'Earth',
-  color: 'silver',
-  'Secret Mission': 'Discover life outside of Earth.'
+// let spaceship = {
+//   'Fuel Type': 'Turbo Fuel',
+//   homePlanet: 'Earth',
+//   color: 'silver',
+//   'Secret Mission': 'Discover life outside of Earth.'
+// };
+
+// spaceship.color = 'Glorious Gold';
+// spaceship.numEngines = 8;
+// delete spaceship['Secret Mission'];
+
+// console.log(spaceship);
+
+// Methods
+
+let retreatMessage =
+  'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage);
+  },
+  takeoff() {
+    console.log('Spim...Borp..Glix...Blastoff!');
+  }
 };
-
-spaceship.color = 'Glorious Gold';
-spaceship.numEngines = 8;
-delete spaceship['Secret Mission'];
-
-console.log(spaceship);
+alienShip.retreat();
+alienShip.takeoff();
